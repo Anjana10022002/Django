f1f9853904b0d64eecfc89fe9cbba8a1adcf5901
Anjana P R <pranjana333@gmail.com>
2025-11-12 22:46:51 +0530

    Fixing errors

--- Django_Lab_Day3_Qn-2/hwday3project/hwday3app/views.py
+++ Django_Lab_Day3_Qn-2/hwday3project/hwday3app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 def student_info(request):
     students = [
-        {'Name': 'Alice', 'Marks': 85, 'Status': 'Pass'},
-        {'Name': 'Bob', 'Marks': 78, 'Status': 'Pass'}, 
-        {'Name': 'Charlie', 'Marks': 92, 'Status': 'Pass'}, 
-        {'Name': 'Eva', 'Marks': 45, 'Status': 'Fail'},
+        {'Name': 'Alice', 'Marks': 85, 'Status': 'pass'},
+        {'Name': 'Bob', 'Marks': 78, 'Status': 'pass'}, 
+        {'Name': 'Charlie', 'Marks': 92, 'Status': 'pass'}, 
+        {'Name': 'Eva', 'Marks': 45, 'Status': 'fail'},
     ]
     student = {'students': students}
     return render(request, 'students.html', student)
