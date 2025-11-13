dd11c610cd07845f5ee790014cb615f12e5b2fd9
Anjana P R <pranjana333@gmail.com>
2025-11-13 23:37:46 +0530

    Implement login form and handle form submission in greeting view

--- todo/todoproject/greeting/views.py
+++ todo/todoproject/greeting/views.py
@@ -1,12 +1,22 @@
-# from django.http import HttpResponse
-# def greeting(request):
-#     return HttpResponse('<h1>Hello world!</h1>')
+# # from django.http import HttpResponse
+# # def greeting(request):
+# #     return HttpResponse('<h1>Hello world!</h1>')
+
+# # from django.shortcuts import render
+# # def greeting(request):
+# #     return render(request,'index.html')
 
 # from django.shortcuts import render
 # def greeting(request):
-#     return render(request,'index.html')
+#     count = 23
+#     return render(request,'index.html',{'count':count})
 
 from django.shortcuts import render
 def greeting(request):
-    count = 23
-    return render(request,'index.html',{'count':count})
+    if request.method == 'POST':
+     email = request.POST.get('email')
+     return render(request,'form-data.html',{
+         'formData':request.POST,
+         'email': email
+     })
+    return render(request,'index.html')
