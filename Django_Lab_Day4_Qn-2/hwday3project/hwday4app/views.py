18473dedb4815f9d7a485ff6319dc665c699d65b
Anjana P R <pranjana333@gmail.com>
2025-11-14 12:33:57 +0530

    Logic for the code

--- Django_Lab_Day4_Qn-2/hwday3project/hwday4app/views.py
+++ Django_Lab_Day4_Qn-2/hwday3project/hwday4app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def userform(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        return render (request, 'user_data_form.html'),{
+            'formData' : request.POST,
+            'name' : name
+        }
+    return render(request, 'user_data_form.html')
