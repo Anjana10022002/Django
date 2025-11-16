7950b8ad31a31872f96869af134b0bb27a7d073d
Anjana P R <pranjana333@gmail.com>
2025-11-16 23:07:50 +0530

    Fixing syntax errors

--- Django_Lab_Day5_Qn-2/hwday5project/hwday5app/views.py
+++ Django_Lab_Day5_Qn-2/hwday5project/hwday5app/views.py
@@ -11,7 +11,8 @@ def register(request):
     if request.method == "POST":
         form = ResourceWarning(request.POST)
         if form.is_valid():
-            return render(request, "success.html", {"name": "full_name"})
+            name = form.cleaned_data['full_name']
+            return render(request, "success.html", {"name": name})
         else:
             form = RegistrationForm()
         return render(request, "registrationpage.html", {"form":form})
