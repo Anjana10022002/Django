0f62e940d55146bbf5e4470e426c030f53aa8740
Anjana P R <pranjana333@gmail.com>
2025-11-16 23:13:24 +0530

    Fixing syntax error

--- Django_Lab_Day5_Qn-2/hwday5project/hwday5app/views.py
+++ Django_Lab_Day5_Qn-2/hwday5project/hwday5app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django import forms
 from .forms import RegistrationForm
 
 def register(request):
     if request.method == "POST":
-        form = ResourceWarning(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['full_name']
             return render(request, "success.html", {"name": name})
         else:
-            form = RegistrationForm()
-        return render(request, "registrationpage.html", {"form":form})
+            return render(request, "registrationpage.html", {"form": form})
+
+    form = RegistrationForm()
+    return render(request, "registrationpage.html", {"form": form})
