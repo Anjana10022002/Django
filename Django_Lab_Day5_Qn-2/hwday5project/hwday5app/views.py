2e7f18c34b051aefb0c33753b1dd4954d6153f20
Anjana P R <pranjana333@gmail.com>
2025-11-16 23:09:27 +0530

    Fixing syntax error

--- Django_Lab_Day5_Qn-2/hwday5project/hwday5app/views.py
+++ Django_Lab_Day5_Qn-2/hwday5project/hwday5app/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from django import forms
-from django.core.validators import validate_email
-
-class RegistrationForm(forms.Form):
-    full_name = forms.CharField(max_length=50, min_length=5)
-    email = forms.CharField(max_length=100,min_length=10, validators=[validate_email])
-    password = forms.CharField(max_length=20, min_length=8)
+from .forms import RegistrationForm
 
 def register(request):
     if request.method == "POST":
