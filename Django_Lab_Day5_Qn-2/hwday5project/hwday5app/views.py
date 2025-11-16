c17a745d86b91a6715dec8cf499db6c9a57e15b5
Anjana P R <pranjana333@gmail.com>
2025-11-16 22:53:22 +0530

    adding registartion form class

--- Django_Lab_Day5_Qn-2/hwday5project/hwday5app/views.py
+++ Django_Lab_Day5_Qn-2/hwday5project/hwday5app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django import forms
+from django.core.validators import validate_email
 
-# Create your views here.
+class RegistrationForm(forms.Form):
+    full_name = forms.CharField(max_length=50, min_length=5)
+    email = forms.CharField(max_length=100,min_length=10, validators=[validate_email])
+    password = forms.CharField(max_length=20, min_length=8)
+
+def register(request):
+    if request.method == "POST":
+        form = ResourceWarning(request.POST)
+        if form.is_valid():
+            name = form.
