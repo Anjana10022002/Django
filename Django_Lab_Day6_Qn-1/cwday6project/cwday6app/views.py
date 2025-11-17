2e28763a800be4252771c669d95a2b0533391481
Anjana P R <pranjana333@gmail.com>
2025-11-17 15:43:11 +0530

    Adding function to views.py

--- Django_Lab_Day6_Qn-1/cwday6project/cwday6app/views.py
+++ Django_Lab_Day6_Qn-1/cwday6project/cwday6app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import MovieForm
 
-# Create your views here.
+def movie_data(request):
+    if request.method == 'POST':
+        form = movie_data(request.POST)
+        if form.is_valid():
+            movie = form.save()       
+            return render(request, 'success.html', {'movie': movie})
+    else:
+        form = MovieForm()
+    return render(request, 'movie_data.html', {'form': form})
+
+    
