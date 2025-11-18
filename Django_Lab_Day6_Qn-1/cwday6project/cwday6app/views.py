ed394f48f38e3c8aa92bb8e54010c251595754c2
Anjana P R <pranjana333@gmail.com>
2025-11-18 22:26:37 +0530

    Fixing syntax error

--- Django_Lab_Day6_Qn-1/cwday6project/cwday6app/views.py
+++ Django_Lab_Day6_Qn-1/cwday6project/cwday6app/views.py
@@ -3,6 +3,7 @@ from .forms import MovieForm
 from .models import Movie
 
 def movie_data(request):
+    message = ""
     if request.method == 'POST':
         form = MovieForm(request.POST)
         if form.is_valid():
@@ -10,7 +11,7 @@ def movie_data(request):
             mov.movie_name = form.cleaned_data["movie_name"]
             mov.release_year = form.cleaned_data["release_year"]
             mov.save()
-            return render(request, 'success.html', {'movie': Movie})
+            return render(request, 'success.html', {'movie': mov})
     else:
         form = MovieForm()
     return render(request, 'movie_data.html', {'form': form})
