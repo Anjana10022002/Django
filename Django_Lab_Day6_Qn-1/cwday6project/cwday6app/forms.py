83169ce6509c1d7d1d99d901971b57bbe7729e3e
Anjana P R <pranjana333@gmail.com>
2025-11-18 22:24:04 +0530

    Fixing syntax error

--- Django_Lab_Day6_Qn-1/cwday6project/cwday6app/forms.py
+++ Django_Lab_Day6_Qn-1/cwday6project/cwday6app/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import Movie
 
-class MovieForm(forms.ModelForm):
-    class Meta:
-        model = Movie
-        fields = ['movie_name', 'release_year']
+class MovieForm(forms.Form):
+    movie_name = forms.CharField(max_length=100, min_length=5)
+    release_year = forms.IntegerField()
--- Django_Lab_Day6_Qn-1/cwday6project/cwday6app/views.py
+++ Django_Lab_Day6_Qn-1/cwday6project/cwday6app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .forms import MovieForm
+from .models import Movie
 
 def movie_data(request):
     if request.method == 'POST':
         form = MovieForm(request.POST)
         if form.is_valid():
-            movie = form.save()
-            return render(request, 'success.html', {'movie': movie})
+            mov = Movie()
+            mov.movie_name = form.cleaned_data["movie_name"]
+            mov.release_year = form.cleaned_data["release_year"]
+            mov.save()
+            return render(request, 'success.html', {'movie': Movie})
     else:
         form = MovieForm()
     return render(request, 'movie_data.html', {'form': form})
