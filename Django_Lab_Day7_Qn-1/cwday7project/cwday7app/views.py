67cf6430bd4df25ac51749e017519d104f50887c
Anjana P R <pranjana333@gmail.com>
2025-11-18 22:38:26 +0530

    Add book view and form for saving book data

--- Django_Lab_Day7_Qn-1/cwday7project/cwday7app/views.py
+++ Django_Lab_Day7_Qn-1/cwday7project/cwday7app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from . import libraryForm
 
-# Create your views here.
+def book(request):
+    if request.method == 'POST':
+        form = libraryForm(request.POST)
+        if form.is_valid():
+            books = form.save()
+            return render(request, 'saved_books.html',
+                          {'message': 'Data saved to db', 
+                          'book_data':books})
+    else:
+        form = libraryForm()
+    return render(request, 'index.html', {'form':form})
