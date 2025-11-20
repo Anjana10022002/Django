1262f6af23f6d50a8d2ac996141d1e27323a46da
Anjana P R <pranjana333@gmail.com>
2025-11-21 00:34:42 +0530

    Fix rendering of student and teacher lists in templates; correct view function return statement

--- Django_Lab_Day9_Qn-2/hwday9project/teachers/views.py
+++ Django_Lab_Day9_Qn-2/hwday9project/teachers/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 def teachers_list(request):
     teachers = ["Mr. Smith", "Ms. Johnson", "Mr. Lee"]
-    return request(request, "teachers/teachers_list.html", {"teachers":teachers})
-
+    return render(request, "teachers/teachers_list.html", {"teachers": teachers})
 # def teachers_list(request):
 #     teachers = Teacher.objects.all()
 #     return render(request, "teachers/teachers_list.html", {"teachers": teachers})
