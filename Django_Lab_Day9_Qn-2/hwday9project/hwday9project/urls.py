5721f8a531b0700d967e681d1cd0834f935ca378
Anjana P R <pranjana333@gmail.com>
2025-11-20 23:06:30 +0530

    Implement home, student list, and teachers list templates; update URL routing and views

--- Django_Lab_Day9_Qn-2/hwday9project/hwday9project/urls.py
+++ Django_Lab_Day9_Qn-2/hwday9project/hwday9project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from students import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('teachers/', include('teachers.urls')),
     path('students/', include('students.urls')),
 ] 
