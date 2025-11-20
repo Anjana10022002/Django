4eb9bff41628d6c5a659a9c08cad9bfc0b15c7f3
Anjana P R <pranjana333@gmail.com>
2025-11-20 22:54:46 +0530

    fixing url issues

--- Django_Lab_Day9_Qn-2/hwday9project/students/views.py
+++ Django_Lab_Day9_Qn-2/hwday9project/students/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def student_list(request):
     students = ['Alice', 'Bob', 'Charlie']
     return render(request, 'students/student_list.html', {'students': students})
+
+def home(request):
+    return render(request, 'students/home.html')
--- Django_Lab_Day9_Qn-2/hwday9project/teachers/urls.py
+++ Django_Lab_Day9_Qn-2/hwday9project/teachers/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from teachers import views
 
-app_name = 'teachers'
-
-urlpattern = [
-    path('', views.teachers_list, name = "teachers_list")
+urlpatterns = [
+    path("", views.teachers_list, name="teachers_list"),
 ]
