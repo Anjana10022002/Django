327b94016b7e650838473a54da540357260f2091
Anjana P R <pranjana333@gmail.com>
2025-11-22 23:45:52 +0530

    updating views.py

--- Django_Lab_Day10_Qn-2/hwday10project/hwday10app/views.py
+++ Django_Lab_Day10_Qn-2/hwday10project/hwday10app/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import studentform
+from .models import student_record
+
+def student_list(request):
+    
 
 def add_student(request):
-    return render(request, "add_student.html")
-
-def 
+    form = studentform(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('student_list')
+    return render(request,'add_student.html', {"students":students})
