b184cc4151af7bc7254d006ca641c40bb92da077
Anjana P R <pranjana333@gmail.com>
2025-11-22 23:55:37 +0530

    Creating update student list logic

--- Django_Lab_Day10_Qn-2/hwday10project/hwday10app/views.py
+++ Django_Lab_Day10_Qn-2/hwday10project/hwday10app/views.py
@@ -6,10 +6,20 @@ def student_list(request):
     student = student_record.objects.all()
     return render(request, 'student_list.html', {'students': student})
 
-
 def add_student(request):
     form = studentform(request.POST)
     if form.is_valid():
         form.save()
         return redirect('student_list')
-    return render(request,'add_student.html', {"students":students})
+    return render(request,'add_student.html', {"students": students})
+
+def edit_students(request):
+    student = student_record.objects.get(pk=id)
+    if request.method == 'POST':
+        form = studentform(request.POST,instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form =ProductForm(instance=product)           
+    return render(request, 'update.html', {'form': form})
