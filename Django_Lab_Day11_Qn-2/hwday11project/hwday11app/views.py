25d0234cad92447d60fb38731a2ecea9ffbc51e0
Anjana P R <pranjana333@gmail.com>
2025-11-24 23:46:30 +0530

    Fixing syntax error

--- Django_Lab_Day11_Qn-2/hwday11project/hwday11app/views.py
+++ Django_Lab_Day11_Qn-2/hwday11project/hwday11app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
-def home(request):
+def home_page(request):
     return render(request, 'home.html')
-def signup(request):
+def signup_page(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -13,15 +14,16 @@ def signup(request):
         form = UserCreationForm()
     return render(request, "signup.html", {"form": form})
 
-def login(request):
+def login_page(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('home')
-        else:
-            form = AuthenticationForm()
-        return render(request, 'login.html', {'form':form})
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form':form})
     
 
--- Django_Lab_Day11_Qn-2/hwday11project/hwday11project/urls.py
+++ Django_Lab_Day11_Qn-2/hwday11project/hwday11project/urls.py
@@ -20,8 +20,8 @@ from hwday11app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name = 'login')
+    path('', views.home_page, name='home'),
+    path('signup/', views.signup_page, name='signup'),
+    path('login/', views.login_page, name = 'login')
 
 ]
