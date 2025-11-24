e2d924d68f9e1d23bb4f4df729fff56da3b8a7f9
Anjana P R <pranjana333@gmail.com>
2025-11-24 23:49:28 +0530

    Creating login form

--- Django_Lab_Day11_Qn-2/hwday11project/hwday11app/views.py
+++ Django_Lab_Day11_Qn-2/hwday11project/hwday11app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
 def home_page(request):
     return render(request, 'home.html')
@@ -25,5 +26,11 @@ def login_page(request):
         form = AuthenticationForm()
 
     return render(request, 'login.html', {'form':form})
+
+@login_required(login_url='/login/')
+def logout_page(requqest):
+    
+
+
     
 
