0afe229be3c02a2c2671d27d5c21ba31afc85599
Anjana P R <pranjana333@gmail.com>
2025-11-24 23:11:01 +0530

    Creating login form

--- Django_Lab_Day11_Qn-2/hwday11project/hwday11app/views.py
+++ Django_Lab_Day11_Qn-2/hwday11project/hwday11app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def signup(request):
     if request.method == 'POST':
@@ -10,3 +10,8 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, "signup.html")
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+S
