7dc0b4ede13b311d7bc0fbad1e3f922b9facc916
Anjana P R <pranjana333@gmail.com>
2025-11-25 23:22:05 +0530

    Creating new api

--- Django_Lab_Day12_Qn-2/hwday12project/hwday12app/views.py
+++ Django_Lab_Day12_Qn-2/hwday12project/hwday12app/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import api_view, permission_classes
+from rest_framework import status
+from rest_framework import 
