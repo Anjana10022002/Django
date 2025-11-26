02bb48e7ef8b08159f4e191499d5bf1f65bc7970
Anjana P R <pranjana333@gmail.com>
2025-11-26 20:17:22 +0530

    Created user signup form

--- Django_Lab_Day13_Qn-2/hwday13project/studentapi/views.py
+++ Django_Lab_Day13_Qn-2/hwday13project/studentapi/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes, AllowwAny
+from rest_framework.response import response
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes((AllowAny,))
+def signup(request):
+    form = UserCreationForm(data=request.data)
+    if form.is_valid():
+        user = form.save()
+        return response("Account created succesfully ", status=status.HTTP_201_CREATED)
+    return response(form.errors, status=status.HTTP_400_BAD_REQUESTs)
