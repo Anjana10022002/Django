20b4e0594afb0d782a724a691ff9e2618a8ad041
Anjana P R <pranjana333@gmail.com>
2025-11-26 19:46:35 +0530

    View data inside database using CRUD

--- todo/todoproject/products/forms.py
+++ todo/todoproject/products/forms.py
@@ -1,6 +1,15 @@
+# from django import forms
+# from .models import Customer
+# class LoginModelForm(forms.ModelForm):
+#     class Meta:
+#         model = Customer
+#         fields = ['email', 'password']
+
+
 from django import forms
-from .models import Customer
-class LoginModelForm(forms.ModelForm):
+from .models import Product
+
+class ProductForm(forms.ModelForm):
     class Meta:
-        model = Customer
-        fields = ['email', 'password']
+        model = Product
+        fields = ['name','description','price']
--- todo/todoproject/productsapi/views.py
+++ todo/todoproject/productsapi/views.py
@@ -24,3 +24,48 @@ def signup(request):
         user = form.save()
         return Response("account created successfully", status=status.HTTP_201_CREATED)
     return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+
+from products.forms import ProductForm
+@api_view(['POST'])
+@permission_classes((AllowAny,))
+def create_product(request):
+    form = ProductForm(request.POST)
+    if form.is_valid():
+        product = form.save()
+        return Response({'id': product.id}, status=status.HTTP_201_CREATED)
+    return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from products.forms import ProductForm
+from products.models import Product
+from .serializers import ProductSerializer
+
+@api_view(['GET'])
+@permission_classes((AllowAny,))
+def list_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes((AllowAny,))
+def update_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    form = ProductForm(request.data, instance=product)
+    if form.is_valid():
+        form.save()
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    else:
+        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+    
