c1bcca5486fcc44181b8e1e56373429ff54ccb7a
Anjana P R <pranjana333@gmail.com>
2025-11-27 21:55:45 +0530

    Creating pdf

--- Django_Lab_Day14_Qn-2/hwday14project/hwday14app/urls.py
+++ Django_Lab_Day14_Qn-2/hwday14project/hwday14app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from hwday14app import views  
 
 urlpatterns = [
+    path('', views.create_product, name='home'),
+    path('create_product/', views.create_product, name='create'),
     path('admin/', admin.site.urls),
-    path('create_product/', views.create_product,name='create')
 ]
--- Django_Lab_Day14_Qn-2/hwday14project/hwday14app/views.py
+++ Django_Lab_Day14_Qn-2/hwday14project/hwday14app/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render, redirect
 from .forms import ProductForm
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from io import BytesIO
+from .models import Product
 
 def create_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
-        else:
-            form = ProductForm()
-        return render(request, 'create.html', {'form':form})
+            return redirect('create')
+    else:
+        form = ProductForm()
+
+    return render(request, 'create.html', {'form': form})
+
+def generate_pdf(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    template = get_template.render('product_pdf.html')
+    html = template.render({'product':product})
+    buffer = BytesIO()
+    pisa_status = pisa.CreatePDF(html, dest=buffer)
+    if pisa_status.error:
+        return HttpResponse('PDF creation error!!')
+    else:
+        response = HttpResponse(buffer.getvalue(), content_type = 'application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="{}.pdf"'.format(product.name)
+        return response
