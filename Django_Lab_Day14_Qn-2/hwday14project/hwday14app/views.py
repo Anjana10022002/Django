de07b7f04b903fb9ae625555f815f9b6ec0f1d47
Anjana P R <pranjana333@gmail.com>
2025-11-28 12:50:21 +0530

    Sending mail to mailtrap

--- Django_Lab_Day14_Qn-2/hwday14project/hwday14app/views.py
+++ Django_Lab_Day14_Qn-2/hwday14project/hwday14app/views.py
@@ -1,3 +1,4 @@
+from itertools import product
 from django.shortcuts import render, redirect
 from .forms import ProductForm
 from django.shortcuts import get_object_or_404, render
@@ -14,24 +15,25 @@ def create_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('product_details')
+            product = form.save()
+            return redirect('product_details', pk=product.pk)
     else:
         form = ProductForm()
     return render(request, 'create.html', {'form': form})
 
 def generate_pdf(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    template = get_template.render('product_pdf.html')
-    html = template.render({'product':product})
+    template = get_template('product_pdf.html')
+    html = template.render({'product': product})
     buffer = BytesIO()
+
     pisa_status = pisa.CreatePDF(html, dest=buffer)
-    if pisa_status.error:
-        return HttpResponse('PDF creation error!!')
-    else:
-        response = HttpResponse(buffer.getvalue(), content_type = 'application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="{}.pdf"'.format(product.name)
-        return response
+
+    if pisa_status.err:
+        return HttpResponse(html)   # Show error inside browser
+    response = HttpResponse(buffer.getvalue(), content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{product.name}.pdf"'
+    return response
     
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
