eca2290cb3d4cec742ac5ec4a707bd82e8a5c0aa
Anjana P R <pranjana333@gmail.com>
2025-11-29 20:36:35 +0530

    Fixing errors

--- Django_Lab_Day14_Qn-2/hwday14project/certificates/urls.py
+++ Django_Lab_Day14_Qn-2/hwday14project/certificates/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from certificates import views
 urlpatterns = [
-    path('',views.certificate_create,name='create'),
+    # path('',views.certificate_create,name='create'),
     path('download_pdf', views.download_pdf, name='download_pdf'),
     # path('send_mail', views.send_mail, name ='send_mail'),
     path('certificate/<int:pk>/pdf/', views.download_pdf, name='download_pdf'),
-    path('certificate/<int:pk>/', views.cert_details_view, name='cert_details'),
+    path('', views.certificate_create, name='certificate_create'),
 
 ]
