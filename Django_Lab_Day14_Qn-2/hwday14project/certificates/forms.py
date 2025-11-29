18602e3d0bf9e19a6156a24c9ee94a1103667637
Anjana P R <pranjana333@gmail.com>
2025-11-29 20:29:16 +0530

    creating form

--- Django_Lab_Day14_Qn-2/hwday14project/certificates/forms.py
+++ Django_Lab_Day14_Qn-2/hwday14project/certificates/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import certificates
+from .models import Certificate
 
 class CertificateForm(forms.ModelForm):
     class Meta:
-        model = certificates
+        model = Certificate
         fields = '__all__'
--- Django_Lab_Day14_Qn-2/hwday14project/certificates/urls.py
+++ Django_Lab_Day14_Qn-2/hwday14project/certificates/urls.py
@@ -3,5 +3,5 @@ from certificates import views
 urlpatterns = [
     path('',views.certificate_create,name='create'),
     path('download_pdf', views.download_pdf, name='download_pdf'),
-    path('send_mail', views.send_mail, name ='send_mail'),
+    # path('send_mail', views.send_mail, name ='send_mail'),
 ]
