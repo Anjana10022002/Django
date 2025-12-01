fa088c2d55427e08f6047f21a371b2f37fac1cc1
Anjana P R <pranjana333@gmail.com>
2025-12-01 23:24:14 +0530

    Creating signup form

--- Django_Machine_Test/mtproject/URLShortner/views.py
+++ Django_Machine_Test/mtproject/URLShortner/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 def signup_page(request):
-    if request.method =='POST'
-    from = User
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form':form})
+                
