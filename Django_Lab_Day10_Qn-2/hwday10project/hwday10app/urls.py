8007b911f67363d2489bc06c96b411e2c9ca565e
Anjana P R <pranjana333@gmail.com>
2025-12-01 15:24:15 +0530

    fixing syntax error

--- Django_Lab_Day10_Qn-2/hwday10project/hwday10app/urls.py
+++ Django_Lab_Day10_Qn-2/hwday10project/hwday10app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from hwday10app import views
 
 urlpatterns = [
-    path('', views.student_list, name='student_list'),  
+    path('', views.student_list, name='home'),
+    path('students/', views.student_list, name='student_list'),  
     path('add/', views.add_student, name='add_student'),
     path('edit/<int:id>/', views.edit_students, name='edit_students'),
     path('delete/<int:id>/', views.delete_student, name='delete_student'),
+
 ]
