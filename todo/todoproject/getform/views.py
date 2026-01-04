6631bce3992aa56433d444dd0b250ff9c50dacf2
Anjana P R <pranjana333@gmail.com>
2026-01-04 23:30:58 +0530

    Refactor form handling: reintroduce LoginForm and update greeting view for POST requests

--- todo/todoproject/getform/views.py
+++ todo/todoproject/getform/views.py
@@ -17,3 +17,13 @@ def greeting(request):
             'email': email
         })
     return render(request,'index.html' )
+
+from django.shortcuts import render
+from .forms import LoginForm
+def greeting(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        return render(request,'form-data.html',{
+            'email': form['email'].value
+        })
+    return render(request,'index.html')
