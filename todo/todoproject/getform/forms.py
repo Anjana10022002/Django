d6c7f48b136389aade82d586461c178f9c2b3425
Anjana P R <pranjana333@gmail.com>
2026-01-04 23:31:29 +0530

    Validating form using form field class properties

--- todo/todoproject/getform/forms.py
+++ todo/todoproject/getform/forms.py
@@ -1,5 +1,9 @@
+# from django import forms
+# class LoginForm(forms.Form):
+#     email = forms.CharField()
+#     password = forms.CharField()
+ 
 from django import forms
 class LoginForm(forms.Form):
-    email = forms.CharField()
-    password = forms.CharField()
- 
+    email = forms.CharField(max_length=100,min_length=10)
+    password = forms.CharField(max_length=50,min_length=6)
