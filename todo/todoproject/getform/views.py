c84c74b00d3c8b20e67da01b5631952a60dab07d
Anjana P R <pranjana333@gmail.com>
2026-01-04 23:27:26 +0530

    Getting data from Post Form

--- todo/todoproject/getform/views.py
+++ todo/todoproject/getform/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
-def getform(request):
-    return render(request, 'getform/getform.html')  
+def greeting(request):
+    if request.method == 'POST':
+     email = request.POST.get('email')
+     return render(request,'form-data.html',{
+         'formData':request.POST,
+         'email': email
+     })
+    return render(request,'index.html')
