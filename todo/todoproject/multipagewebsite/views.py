db049dbf58c52bb47effb3993536991cf503aa83
Anjana P R <pranjana333@gmail.com>
2026-01-05 23:38:38 +0530

    Added logic in views.py

--- todo/todoproject/multipagewebsite/views.py
+++ todo/todoproject/multipagewebsite/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import LoginModelForm
 
-# Create your views here.
+def greeting(request):
+    if request.method == 'POST':
+        form = LoginModelForm(request.POST)
+        if form.is_valid():
+            cust = form.save()
+            return render(request,'form-data.html',{
+                'message': 'Data saved to db',
+                'customer': cust
+            })
+    else:
+        form = LoginModelForm()
+    return render(request,'index.html',{'form':form})
+
+def aboutUs(request):
+    return render(request,'about-us.html')
